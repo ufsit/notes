5b4c4b14dd20cd0e325c8c23a60e3daa8c183632
Doiran <grim_gravewalker@protonmail.com>
2026-08-28 23:57:33 -0400

    Implemented the WAF deployment with caddy + Corza/CRS deployed in the front server. Will work on later to ensure it does not fail any edge cases.

--- blue/Tools/zoo/modules/turtle/turtle.sh
+++ blue/Tools/zoo/modules/turtle/turtle.sh
@@ -4,7 +4,7 @@
 # the target's web-server footprint so the WAF can be slotted in front of it.
 set -euo pipefail
 
-echo("⠀⢀⣒⠒⠆⠤⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+echo "⠀⢀⣒⠒⠆⠤⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 ⢠⡛⠛⠻⣷⣶⣦⣬⣕⡒⠤⢀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 ⡿⢿⣿⣿⣿⣿⣿⡿⠿⠿⣿⣳⠖⢋⣩⣭⣿⣶⡤⠶⠶⢶⣒⣲⢶⣉⣐⣒⣒⣒⢤⡀⠀⠀⠀⠀⠀⠀⠀
 ⣿⠀⠉⣩⣭⣽⣶⣾⣿⢿⡏⢁⣴⠿⠛⠉⠁⠀⠀⠀⠀⠀⠀⠉⠙⠲⢭⣯⣟⡿⣷⣘⠢⡀⠀⠀⠀⠀⠀
@@ -29,8 +29,8 @@ echo("⠀⢀⣒⠒⠆⠤⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀�
 ⠀⠀⠀⠀⠀⠀⠀⢠⣎⡀⢀⣾⠇⢀⣠⡶⢶⠞⠋⠉⠉⠒⢄⡀⠉⠈⠉⠀⠀⠀⣠⣾⠀⠀⠀⠀⠀⢸⡀
 ⠀⠀⠀⠀⠀⠀⠀⠘⣦⡀⠘⢁⡴⢟⣯⣞⢉⠀⠀⠀⠀⠀⠀⢹⠶⠤⠤⡤⢖⣿⡋⢇⠀⠀⠀⠀⠀⢸⠀
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠵⠗⠺⠟⠖⢈⡣⡄⠀⠀⠀⠀⢀⣼⡤⣬⣽⠾⠋⠉⠑⠺⠧⣀⣤⣤⡠⠟⠃
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠛⠷⠶⠦⠶⠞⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀")
-echo("ᗜ‿ᗜ I froze the turtle ᗜ‿ᗜ")
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠛⠷⠶⠦⠶⠞⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
+echo "ᗜ‿ᗜ | I froze the turtle. | ᗜ‿ᗜ"
 
 # Might update for better readability
 HERE="$(cd "$(dirname "$0")" && pwd)"
@@ -41,10 +41,34 @@ printf "\nSaved: %s\n" "$log"
 
 # Not sure if this ^ checks for caddy deployment so:
 if ! command -v caddy &>/dev/null; then
-    echo "ᗜ‿ᗜ Error: 'caddy' binary not found. Install Caddy compiled with coraza-caddy. ᗜ‿ᗜ"
+    echo "ᗜ‿ᗜ | Error: 'caddy' binary not found. Install Caddy compiled with coraza-caddy. | ᗜ‿ᗜ"
     exit 1
 fi 
-
 if [ ! -f "$HERE/coraza.conf-recommended" ]; then
     curl -sSL "https://raw.githubusercontent.com/corazawaf/coraza/main/coraza.conf-recommended" -o "$HERE/coraza.conf-recommended"
 fi
+if [ ! -d "$HERE/coreruleset" ]; then
+    git clone --depth 1 https://github.com/coreruleset/coreruleset.git "$HERE/coreruleset"
+fi
+
+echo "ᗜ‿ᗜ | Generating Caddy... | ᗜ‿ᗜ"
+python3 "$HERE/configgen.py"
+
+if [ ! -f "$HERE/Caddyfile" ]; then
+    echo "ᗜ‿ᗜ | Error: Caddy Generation failed. | ᗜ‿ᗜ"
+    exit 1
+fi 
+
+echo "ᗜ‿ᗜ | Validating & Starting Caddy WAF. | ᗜ‿ᗜ"
+
+caddy validate --config "$HERE/Caddyfile"
+caddy start --config "$HERE/Caddyfile"
+# Maybe find a way to grab external port from config in caddyfile using grep...
+read -p "Enter external port configured in Caddyfile: " EXT_PORT
+
+echo -e "\n ❄️ Testing legitimate traffic proxing (Expect 200 OK or app response...)"
+curl -s -i "http://127.0.0.1:${EXT_PORT}/" | head -n 5
+echo -e "\n ❄️ Testing attack filtering (Expect 403 Forbidden...)"
+curl -s -i "http://127.0.0.1:${EXT_PORT}/?id=1%20UNION%20SELECT%201,2,3" | head -n 5
+
+echo -e "\nᗜ‿ᗜ | Caddy-WAF Deployment Finished | ᗜ‿ᗜ"
